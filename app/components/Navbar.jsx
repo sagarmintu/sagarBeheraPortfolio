46e97ecb4c1736271b2cd1cf366f76917066e533
sagarmintu <sagarskbehera1996@gmail.com>
2025-06-03 11:26:34 +0530

    added navbar and header section as per the figma

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,26 +1,63 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
 
 const Navbar = () => {
+
+    const sideMenuRef = useRef(); 
+
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
   return (
     <>
-      <nav>
-        <a href="">
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt='' className='w-full' />
+    </div>
+      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+        <a href="#top">
             <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt="username logo" />
         </a>
 
-        <ul>
-            <li><a href="#top">Home</a></li>
-            <li><a href="#about">About me</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#work">My Work</a></li>
-            <li><a href="#contact">Contact me</a></li>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 bg-white shadow-sm bg-opacity-50'>
+            <li><a className='navbar_name' href="#top">Home</a></li>
+            <li><a className='navbar_name' href="#about">About me</a></li>
+            <li><a className='navbar_name' href="#services">Services</a></li>
+            <li><a className='navbar_name' href="#work">My Work</a></li>
+            <li><a className='navbar_name' href="#contact">Contact me</a></li>
         </ul>
 
-        <div>
-            <a href="#contact">Contact <Image src={assets.arrow_icon} className='w-3' alt="arrow sign" /></a>
+        <div className='flex items-center gap-4'>
+            <button>
+                <Image src={assets.moon_icon} alt='' className='w-6' />
+            </button>
+
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 navbar_name'>Contact <Image src={assets.arrow_icon} className='w-3' alt="arrow sign" /></a>
+
+            <button className='block md:hidden ml-3'>
+                <Image src={assets.menu_black} alt='' className='w-6' onClick={openMenu} />
+            </button>
         </div>
+
+        {/* for mobile menu */}
+
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+            <div className='absolute right-6 top-6' onClick={closeMenu}>
+                <Image src={assets.close_black} alt='' className='w-5 cursor-pointer' />
+            </div>
+
+            <li><a className='navbar_name' onClick={closeMenu} href="#top">Home</a></li>
+            <li><a className='navbar_name' onClick={closeMenu} href="#about">About me</a></li>
+            <li><a className='navbar_name' onClick={closeMenu} href="#services">Services</a></li>
+            <li><a className='navbar_name' onClick={closeMenu} href="#work">My Work</a></li>
+            <li><a className='navbar_name' onClick={closeMenu} href="#contact">Contact me</a></li>
+        </ul>
 
       </nav>
     </>
