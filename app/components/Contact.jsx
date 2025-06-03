92aaafd4bc227e5f315d933a5d2c1fdf35c0a9b9
sagarmintu <sagarskbehera1996@gmail.com>
2025-06-03 18:20:22 +0530

    added the footer section as well as contact section

---
+++ app/components/Contact.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+
+const Contact = () => {
+
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "6e7c16ae-cac6-4fe2-853b-551ee65d29f9");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <>
+      <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[ul("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+        <h4 className='text-center mb-2 text-lg navbar_name'>Connect with me</h4>
+        <h2 className='text-center text-5xl navbar_name'>Get in touch</h2>
+        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 navbar_name'>I'd love to hear from you! If you have any questions, comments, or feedback, please use the form below.</p>
+
+          <form className='max-w-2xl mx-auto' onSubmit={onSubmit}>
+            <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 mt-10 mb-8'>
+                <input type="text" className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' placeholder="Enter Your Name" name='name' required />
+                <input type="email" className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' placeholder="Enter Your Email" name='email' required />
+            </div>
+            <textarea rows="6" className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' placeholder="Enter Your Message" name='message'></textarea>
+
+            <button type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto duration-500 hover:bg-[#000]'>Submit now <Image src={assets.right_arrow_white} alt='' className='w-4' /></button>
+
+            <p className=''>{result}</p>
+
+          </form>
+      </div>
+    </>
+  )
+}
+
+export default Contact
---
+++ app/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <>
+        <div className='mt-20'>
+            <div className='text-center'>
+                <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2' />
+                <div className='w-max flex items-center gap-2 mx-auto'>
+                    <Image src={assets.mail_icon} alt='' className='w-6' />sagarskbehera1996@gmail.com
+                </div>
+            </div>
+            <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+                <p>@2025 Sagar Behera. All rights reserved.</p>
+                <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                    <li><a target='_blank' href="https://github.com/sagarmintu">Github</a></li>
+                    <li><a target='_blank' href="https://www.linkedin.com/in/sagar-kumar-behera-b19a88146/">Linkdin</a></li>
+                    <li><a target='_blank' href="#">Twitter</a></li>
+                </ul>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer
